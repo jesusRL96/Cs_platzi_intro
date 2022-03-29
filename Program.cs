560d85887f6c9fe0c826737df5bd09a95d00cc72
jesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-03-29 10:16:59 -0600

    Introducción a la Programación Orientada a Objetos

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CsPlatziIntro
+{
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+
+        public string ceo;
+
+        public void Timbrar()
+        {
+            // TODO
+            Console.Beep(1000, 1000);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "platzi";
+            miEscuela.direccion = "calle falsa 123";
+            miEscuela.añoFundacion = 2019;
+            Console.WriteLine($"Nombre: {miEscuela.nombre}, Timbrar");
+            miEscuela.Timbrar();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Fundamentos de C# con NET Core
+Aplicacion de consola con C# (Escuela)
